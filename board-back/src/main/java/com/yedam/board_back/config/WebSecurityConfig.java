14dd13b7d56f616d22978c9f0f1d03f86cbbef5b
kyungminsu <minsu980407@naver.com>
2024-08-20 16:45:37 +0900

    로그인 api 구현, SpringBoot3 마이그레이션

--- board-back/src/main/java/com/yedam/board_back/config/WebSecurityConfig.java
+++ board-back/src/main/java/com/yedam/board_back/config/WebSecurityConfig.java
@@ -7,11 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.yedam.board_back.filter.JwtAuthenticationFilter;
 
@@ -28,22 +33,54 @@ public class WebSecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable()) // CSRF 비활성화
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리
-            .authorizeHttpRequests(auth -> auth
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .cors(cors -> cors
+                .configurationSource(corsConfigurationSource())
+            )
+            .csrf(CsrfConfigurer::disable)
+            .httpBasic(HttpBasicConfigurer::disable)
+            .sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(request -> request
                 .requestMatchers("/", "/api/v1/auth/**", "/api/v1/search/**", "/file/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/board/**", "/api/v1/user/**").permitAll()
-                .anyRequest().authenticated()) // 요청에 대한 권한 설정
-            .exceptionHandling(exception -> exception
-                .authenticationEntryPoint(new FailedAuthenticationEntryPoint())) // 인증 실패 시 처리
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling(exceptionHandling -> exceptionHandling
+            .authenticationEntryPoint(new FailedAuthenticationEntryPoint()) // 인증 실패 시 처리
+            )
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+            
+            // 아래버전일 때
+            // .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+            // .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리
+            // .authorizeHttpRequests(auth -> auth
+            //     .requestMatchers("/", "/api/v1/auth/**", "/api/v1/search/**", "/file/**").permitAll()
+            //     .requestMatchers(HttpMethod.GET, "/api/v1/board/**", "/api/v1/user/**").permitAll()
+            //     .anyRequest().authenticated()) // 요청에 대한 권한 설정
+            // .exceptionHandling(exception -> exception
+            //     .authenticationEntryPoint(new FailedAuthenticationEntryPoint())) // 인증 실패 시 처리
+
+            httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
 
         // CORS 설정을 추가하고, 더 이상 .cors()를 직접 호출하지 않습니다.
-        return http.build();
+        return httpSecurity.build();
     }
 
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
+    }
 }
 
 class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
--- board-back/src/main/java/com/yedam/board_back/provider/JwtProvider.java
+++ board-back/src/main/java/com/yedam/board_back/provider/JwtProvider.java
@@ -3,13 +3,15 @@ package com.yedam.board_back.provider;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
-
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtProvider {
@@ -20,9 +22,9 @@ public class JwtProvider {
     public String create(String email){
 
         Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
-
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
         String jwt = Jwts.builder()
-            .signWith(SignatureAlgorithm.ES256, secretKey)
+            .signWith(key, SignatureAlgorithm.HS256)
             .setSubject(email).setIssuedAt(new Date()).setExpiration(expiredDate)
             .compact();
 
@@ -31,9 +33,14 @@ public class JwtProvider {
 
     public String validate(String jwt){
         Claims claims = null;
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
         try{
-            claims = Jwts.parser().setSigningKey(secretKey)
-                .parseClaimsJws(jwt).getBody();
+            claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
         }catch (Exception exception){
             exception.printStackTrace();
             return null;
