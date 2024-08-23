715146c3a027950e49b63198ab4eaa7d53a18376
kyungminsu <minsu980407@naver.com>
2024-08-23 17:27:18 +0900

    Auth 페이지 회원가입 카드

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -10,19 +10,16 @@ const API_DOMAIN = `${DOMAIN}/api/v1`;
 const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`
 const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`
 
-export const signInRequest = async(requestBody: SignInRequestDto) => {
-    const result = await axios.post<SignInResponseDto>(SIGN_IN_URL(), requestBody)
-        .then(response =>{
-            const responseBody: SignInResponseDto = response.data;
+export const signInRequest = async (requestBody: SignInRequestDto) => {
+    const result = await axios.post(SIGN_IN_URL(), requestBody)
+        .then((response) => {
+            const responseBody = response.data;
             return responseBody;
         })
-        .catch(error =>{
-            if(!error.response.data) return null;
+        .catch((error) => {
+            if(!error.response) return null;
             const responseBody: ResponseDto = error.response.data;
             return responseBody;
-        })
+        });
     return result;
-}
-export const signUpRequest = async(requestBody: SignUpRequestDto) => {
-
 }
--- board-front/src/apis/response/auth/sign-in.responsedto.ts
+++ board-front/src/apis/response/auth/sign-in.responsedto.ts
@@ -2,5 +2,5 @@ import ResponseDto from '../response.dto';
 
 export default interface SignInResponseDto extends ResponseDto{
     token: string;
-    expiratonTime: number;
+    expirationTime: number;
 }
