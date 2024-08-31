80e6fdb9c26d79062e5a5283677884da444f3d2f
kyungminsu <minsu980407@naver.com>
2024-09-01 03:49:36 +0900

    게시물 상세보기 하단 컴포넌트 구조 작성

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css';
+import FavoriteItem from 'components/FavoriteItem';
+import { CommentListItem, FavoriteListItem } from 'types/interface';
+import { commentListMock, favoriteListMock } from 'mocks';
+import CommentItem from 'components/CommentItem';
+import Pagination from 'components/Pagination';
 
 // component: 게시물 상세 화면 컴포넌트 //
 export default function BoardDetail() {
 
-// component: 게시물 상세 상단 컴포넌트 //
-  const BoardDetailTop = () =>{
-    return(
+  // component: 게시물 상세 상단 컴포넌트 //
+  const BoardDetailTop = () => {
+    return (
       <div id='board-detail-top'>
         <div className='board-detail-top-header'>
           <div className='board-detail-title'>{'오늘 머먹지?'}</div>
@@ -37,18 +42,75 @@ export default function BoardDetail() {
   }
 
 // component: 게시물 상세 하단 컴포넌트 //
-  const BoardDetailBottom = () =>{
-    return(
-      <></>
+  const BoardDetailBottom = () => {
+    const [favoriteList, setFavoriteList] = useState<FavoriteListItem[]>([]);
+    const [commentList, setCommentList] = useState<CommentListItem[]>([]);
+    
+    useEffect(()=>{
+      setFavoriteList(favoriteListMock);
+      setCommentList(commentListMock);
+    },[])
+  // render: 게시물 상세 화면 랜더링 //
+    return (
+      <div id='board-detail-bottom'>
+        <div className='board-detail-bottom-button-box'>
+          <div className='board-detail-bottom-button-group'>
+            <div className='icon-button'>
+              <div className='icon favorite-fill-icon'></div>
+            </div>
+            <div className='board-detail-bottom-button-text'>{`좋아요 ${12}`}</div>
+            <div className='icon-button'>
+              <div className='icon up-light-icon'></div>
+            </div>
+          </div>
+          <div className='board-detail-bottom-button-group'>
+            <div className='icon-button'>
+              <div className='icon comment-icon'></div>
+            </div>
+            <div className='board-detail-bottom-button-text'>{`댓글 ${12}`}</div>
+            <div className='icon-button'>
+              <div className='icon up-light-icon'></div>
+            </div>
+          </div>
+        </div>
+        <div className='board-detail-bottom-favorite-box'>
+          <div className='board-detail-bottom-favorite-container'>
+            <div className='board-detail-bottom-favorite-title'>{'좋아요'}<span className='emphasis'>{'12'}</span></div>
+            <div className='board-detail-bottom-favorite-contents'>
+              {favoriteList.map(item=> <FavoriteItem favoriteListItem={item}/>)}
+            </div>
+          </div>
+        </div>
+        <div className='board-detail-bottom-comment-box'>
+          <div className='board-detail-bottom-comment-container'>
+            <div className='board-detail-bottom-comment-title'>{'댓글'}<span className='emphasis'>{'12'}</span></div>
+            <div className='board-detail-bottom-comment-container'>
+              {commentList.map(item => <CommentItem commentListItem={item}/>)}
+            </div>
+          </div>
+          <div className='devider'></div>
+          <div className='board-detail-bottom-comment-pagination-box'>
+            <Pagination />
+          </div>
+          <div className='board-detail-bottom-comment-input-container'>
+            <div className='board-detail-bottom-comment-input-container'>
+              <textarea className='board-detail-bottom-comment-textarea' placeholder='댓글을 작성해주세요.'/>
+              <div className='board-detail-bottom-comment-button-box'>
+                <div className='disable-button'>{'댓글달기'}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 
-// render: 게시물 상세 화면 랜더링 //
+  // render: 게시물 상세 화면 랜더링 //
   return (
     <div id='board-detail-wrapper'>
       <div className='board-detail-container'>
-        <BoardDetailTop/>
-        <BoardDetailBottom/>
+        <BoardDetailTop />
+        <BoardDetailBottom />
       </div>
     </div>
   )
