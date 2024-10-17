cfb9c8be38401d1ca4eb437cb024e767f47053c5
kyungminsu <minsu980407@naver.com>
2024-10-17 16:34:10 +0900

    인기 검색어 리스트 쿼리 공백추가

--- board-back/src/main/java/com/yedam/board_back/repository/SearchLogRepository.java
+++ board-back/src/main/java/com/yedam/board_back/repository/SearchLogRepository.java
@@ -10,17 +10,17 @@ import com.yedam.board_back.entity.SearchLogEntity;
 import com.yedam.board_back.repository.resultSet.GetPopularListResultSet;
 
 @Repository
-public interface SearchLogRepository extends JpaRepository<SearchLogEntity, Integer>{
+public interface SearchLogRepository extends JpaRepository<SearchLogEntity, Integer> {
     
     @Query(
-        value=
-        "SELECT search_word AS searchWord, count(search_word) AS count" +
-        "FROM search_log" +
-        "WHERE relation IS FALSE" +
-        "GROUP BY search_word" +
-        "ORDER BY count DESC" +
+        value =
+        "SELECT search_word AS searchWord, count(search_word) AS count " +
+        "FROM search_log " +
+        "WHERE relation IS FALSE " +
+        "GROUP BY search_word " +
+        "ORDER BY count DESC " +
         "LIMIT 15",
-        nativeQuery=true
+        nativeQuery = true
     )
     List<GetPopularListResultSet> getPopularList();
 
